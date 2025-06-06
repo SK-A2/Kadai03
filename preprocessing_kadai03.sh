03c5bbd76b5e837c21275dc89a636ddbca57918d
SK-A2 <kosuda-s@a2healthcare.com>
2025-06-06 11:40:59 +0900

    5th commit: Modify Parallel command

--- preprocessing_kadai03.sh
+++ preprocessing_kadai03.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-bzcat $1 | 
-parallel --pipe --block 10M  "jq -c '{refsnp_id:.refsnp_id} + 
-	{primary_snapshot_data:.primary_snapshot_data.placements_with_allele[] | select(.is_ptlp==true) | .alleles[] | select(.hgvs|contains(\">\"))}'" |
-parallel --pipe --block 10M "jq -r -c '[.refsnp_id, .primary_snapshot_data.allele.spdi.deleted_sequence, .primary_snapshot_data.allele.spdi.inserted_sequence, .primary_snapshot_data.hgvs[-3:]] | @tsv'" |
+# Split Data
+# bzcat $1 | split -l 800000 -d -a 1 --additional-suffix=.json - splitData_
+
+# Parallel + JQ
+parallel -j 4 "jq -c '{refsnp_id:.refsnp_id} + {primary_snapshot_data:.primary_snapshot_data.placements_with_allele[] | select(.is_ptlp==true) | .alleles[] | select(.hgvs|contains(\">\"))}' {} > _{/}" ::: splitData* 
+
+parallel -j 4 "jq -r -c '[.refsnp_id, .primary_snapshot_data.allele.spdi.deleted_sequence, .primary_snapshot_data.allele.spdi.inserted_sequence, .primary_snapshot_data.hgvs[-3:]] | @tsv' {}" ::: _splitData* |
+
+# Sort
 sort -n -k 1 -t\t > _result.tsv
