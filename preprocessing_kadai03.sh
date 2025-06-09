9e5139e55020f9b215cbb85a8df8d4b153f640ea
SK-A2 <kosuda-s@a2healthcare.com>
2025-06-09 16:02:17 +0900

    7th commit: Modified to increase the number of parallel processes. See Readme.md

--- preprocessing_kadai03.sh
+++ preprocessing_kadai03.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+# Make Directory for Intermediate Data (Delete after Run)
+mkdir _tmp
+
 # Split Data
-time bzcat $1 | split -l 800000 -d -a 1 --additional-suffix=.json - splitData_
+time bzcat $1 | split -l 100000 -d -a 2 --additional-suffix=.json - ./_tmp/splitData
 
 # Parallel + JQ
-time parallel -j 4 "jq -c '{refsnp_id:.refsnp_id} + {primary_snapshot_data:.primary_snapshot_data.placements_with_allele[] | select(.is_ptlp==true) | .alleles[] | select(.hgvs|contains(\">\"))}' {} > _{/}" ::: splitData* 
+time parallel \
+	"jq -c 	'{refsnp_id:.refsnp_id} + \
+		{primary_snapshot_data:.primary_snapshot_data.placements_with_allele[] | \
+		select(.is_ptlp==true) | \
+		.alleles[] | \
+		select(.hgvs|contains(\">\"))}' {} > {.}.tsv.json" \
+	::: ./_tmp/splitData* 
 
-time parallel -j 4 "jq -r -c '[.refsnp_id, .primary_snapshot_data.allele.spdi.deleted_sequence, .primary_snapshot_data.allele.spdi.inserted_sequence, .primary_snapshot_data.hgvs[-3:]] | @tsv' {}" ::: _splitData* |
+time parallel \
+	"jq -r -c 	'[.refsnp_id, \
+			.primary_snapshot_data.allele.spdi.deleted_sequence, \
+			.primary_snapshot_data.allele.spdi.inserted_sequence, \
+			.primary_snapshot_data.hgvs[-3:]] | \
+			@tsv' {} > {.}.tsv"  \
+	::: ./_tmp/*.tsv.json 
 
 # Sort
-sort -n -k 1 -t\t > _result.tsv
+cat ./_tmp/*.tsv | sort -n -k 1 -t\t > ./result.tsv
 
 # Remove Intermediate Data
-rm splitData*
-rm _splitData*
+# rm -r _tmp
